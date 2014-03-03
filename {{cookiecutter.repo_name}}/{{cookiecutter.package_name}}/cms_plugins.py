e590b4bc9b2a9b5fcff537713a818198f2c33c79
Kim Thoenen <kim@smuzey.ch>
2014-03-03 13:36:28 +0100

    Adapted Setup

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/cms_plugins.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/cms_plugins.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.utils.translation import ugettext_lazy as _
-from django.conf.urls import patterns, url
+# from django.utils.translation import ugettext_lazy as _
+# from django.conf.urls import patterns, url
 
-from cms.plugin_base import CMSPluginBase
-from cms.plugin_pool import plugin_pool
+# from cms.plugin_base import CMSPluginBase
+# from cms.plugin_pool import plugin_pool
 
 # from .models import Model
 # from .forms import Form
