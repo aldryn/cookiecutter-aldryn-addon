82079c67c7a0c91e8ecdf2a1d15a8f8c03b7d071
Kim Thoenen <kim@smuzey.ch>
2014-08-14 10:06:18 +0200

    add test environment, shippable config

---
+++ {{cookiecutter.repo_name}}/test_settings.py
@@ -0,0 +1,7 @@
+HELPER_SETTINGS = {
+    'TIME_ZONE': 'Europe/Zurich',
+    'INSTALLED_APPS': [],
+    'NOSE_ARGS': [
+        '--with-xunit', '--xunit-file=shippable/testresults/test.xml',
+    ],
+}
